e2afb0f1cfe7e2e962e1130af943b3e86869acdc
bk201 <111396011+ang-201@users.noreply.github.com>
2023-09-17 01:16:49 -0600

    Ejercicio  (Actualizacion) 321.c

--- 321.c
+++ 321.c
@@ -2,138 +2,102 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Estructura para un nodo de la lista enlazada
-struct Nodo {
-    char palabra[100];
-    struct Nodo* siguiente;
+struct Node {
+    char name[50];
+    struct Node* next;
 };
 
-// Función para crear un nuevo nodo con la palabra dada
-struct Nodo* crearNodo(const char* palabra) {
-    struct Nodo* nuevoNodo = (struct Nodo*)malloc(sizeof(struct Nodo));
-    strcpy(nuevoNodo->palabra, palabra);
-    nuevoNodo->siguiente = NULL;
-    return nuevoNodo;
+struct LinkedList {
+    struct Node* head;
+    struct Node* tail;
+};
+
+void initializeList(struct LinkedList* list) {
+    list->head = NULL;
+    list->tail = NULL;
 }
 
-// Función para insertar un nodo al final de la lista enlazada
-void insertarAlFinal(struct Nodo** cabeza, const char* palabra) {
-    struct Nodo* nuevo = crearNodo(palabra);
-    if (*cabeza == NULL) {
-        *cabeza = nuevo;
+void insert(struct LinkedList* list, const char* name) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    strcpy(newNode->name, name);
+    newNode->next = NULL;
+
+    if (list->head == NULL) {
+        list->head = newNode;
+        list->tail = newNode;
     } else {
-        struct Nodo* actual = *cabeza;
-        while (actual->siguiente != NULL) {
-            actual = actual->siguiente;
-        }
-        actual->siguiente = nuevo;
+        list->tail->next = newNode;
+        list->tail = newNode;
     }
 }
 
-// Función para eliminar un nodo con la palabra dada de la lista
-void eliminarPalabra(struct Nodo** cabeza, const char* palabra) {
-    struct Nodo* actual = *cabeza;
-    struct Nodo* previo = NULL;
+void removeNodesGreaterThan(struct LinkedList* list, const char* name) {
+    struct Node* current = list->head;
+    struct Node* previous = NULL;
 
-    while (actual != NULL) {
-        if (strcmp(actual->palabra, palabra) == 0) {
-            if (previo == NULL) {
-                *cabeza = actual->siguiente;
+    while (current != NULL) {
+        if (strcmp(current->name, name) > 0) {
+            if (previous == NULL) {
+                list->head = current->next;
             } else {
-                previo->siguiente = actual->siguiente;
+                previous->next = current->next;
             }
-            free(actual);
-            return; // Palabra encontrada y eliminada
+            struct Node* temp = current;
+            current = current->next;
+            free(temp);
+        } else {
+            previous = current;
+            current = current->next;
         }
-        previo = actual;
-        actual = actual->siguiente;
+    }
+
+    if (previous == NULL) {
+        list->tail = NULL;
+    } else {
+        list->tail = previous;
     }
 }
 
-// Función para mostrar las palabras en la lista
-void mostrarLista(struct Nodo* cabeza) {
-    struct Nodo* actual = cabeza;
-    while (actual != NULL) {
-        printf("%s\n", actual->palabra);
-        actual = actual->siguiente;
+void printList(struct LinkedList list) {
+    struct Node* current = list.head;
+    while (current != NULL) {
+        printf("%s ", current->name);
+        current = current->next;
     }
+    printf("\n");
 }
 
-// Función para liberar la memoria de la lista enlazada
-void liberarLista(struct Nodo* cabeza) {
-    while (cabeza != NULL) {
-        struct Nodo* temp = cabeza;
-        cabeza = cabeza->siguiente;
+void freeList(struct LinkedList* list) {
+    struct Node* current = list->head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
         free(temp);
     }
+    list->head = NULL;
+    list->tail = NULL;
 }
 
 int main() {
-    FILE* archivo = fopen("palabras.txt", "r"); // Nombre del archivo de texto
-    if (archivo == NULL) {
-        perror("Error al abrir el archivo");
-        return 1;
-    }
+    struct LinkedList list;
+    initializeList(&list);
 
-    struct Nodo* lista = NULL;
-    char palabra[100];
+    insert(&list, "Mar");
+    insert(&list, "Sella");
+    insert(&list, "Centurión");
 
-    // Leer palabras del archivo y construir la lista
-    while (fscanf(archivo, "%s", palabra) == 1) {
-        insertarAlFinal(&lista, palabra);
-    }
+    printf("Elementos de la lista enlazada: ");
+    printList(list);
 
-    fclose(archivo);
+    insert(&list, "Gloria");
+    insert(&list, "Generosa");
 
-    int opcion;
-    do {
-        printf("\n1. Agregar palabra\n");
-        printf("2. Eliminar palabra\n");
-        printf("3. Mostrar palabras\n");
-        printf("4. Salir\n");
-        printf("Seleccione una opción: ");
-        scanf("%d", &opcion);
+    removeNodesGreaterThan(&list, "Centurión");
 
-        switch (opcion) {
-            case 1:
-                printf("Ingrese la palabra a agregar: ");
-                scanf("%s", palabra);
-                insertarAlFinal(&lista, palabra);
-                break;
-            case 2:
-                printf("Ingrese la palabra a eliminar: ");
-                scanf("%s", palabra);
-                eliminarPalabra(&lista, palabra);
-                break;
-            case 3:
-                printf("Palabras en la lista:\n");
-                mostrarLista(lista);
-                break;
-            case 4:
-                break;
-            default:
-                printf("Opción no válida.\n");
-        }
-    } while (opcion != 4);
+    printf("Elementos de la lista después de eliminar mayores a Centurión: ");
+    printList(list);
 
-    // Abrir el archivo para escribir las palabras
-    archivo = fopen("palabras.txt", "w");
-    if (archivo == NULL) {
-        perror("Error al abrir el archivo para escritura");
-        return 1;
-    }
-
-    // Escribir las palabras en el archivo
-    struct Nodo* actual = lista;
-    while (actual != NULL) {
-        fprintf(archivo, "%s\n", actual->palabra);
-        actual = actual->siguiente;
-    }
-
-    fclose(archivo);
-
-    // Liberar la memoria de la lista
-    liberarLista(lista);
+    freeList(&list);
 
     return 0;
 }
